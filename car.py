dc8f0c1df0b2a7d2ba01682ba30d41fac89e7b17
Рома Жуков <romazukov@MacBook-Air-Roma.local>
2023-04-10 19:30:15 +0300

    add car.py

---
+++ car.py
@@ -0,0 +1,20 @@
+class car(object):
+
+    def __init__(self, model, colour, engine):
+        self.model = model
+        self.colour = colour
+        self.engine = engine
+
+    def ride_front():
+        pass
+
+    def ride_back():
+        pass
+
+class car_1(car):
+
+    def turn_left():
+        pass
+
+    def turn_right():
+        pass
